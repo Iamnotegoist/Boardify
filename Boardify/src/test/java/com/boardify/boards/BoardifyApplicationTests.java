2c84832bf5e8df38d40f95088260c70b922d3db3
kiju9512 <kiju9512@gmail.com>
2023-11-15 17:48:16 +0900

    [spring-boot-study]2023-11-15/이기주/Boardify

---
+++ Boardify/src/test/java/com/boardify/boards/BoardifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardify.boards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
